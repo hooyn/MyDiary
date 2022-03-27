0e9e021e094560cd2ecf9e30385ff9538503fe3c
hooyn <hooyn_industry@naver.com>
2022-03-27 09:53:20 +0900

    다이어리 리포지토리, 서비스 개발시작

--- src/main/java/diary/diaryApp/repository/DiaryRepository.java
+++ src/main/java/diary/diaryApp/repository/DiaryRepository.java
@@ -1,4 +1,15 @@
 package diary.diaryApp.repository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
 public class DiaryRepository {
+
+    private final EntityManager em;
+
+
 }
--- src/main/java/diary/diaryApp/service/DiaryService.java
+++ src/main/java/diary/diaryApp/service/DiaryService.java
@@ -1,4 +1,14 @@
 package diary.diaryApp.service;
 
+import diary.diaryApp.repository.DiaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DiaryService {
+
+    private final DiaryRepository diaryRepository;
+
+
 }
